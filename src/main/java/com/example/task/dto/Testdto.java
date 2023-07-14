ac9ee387ee77432f2e3fbadbd5ef49b91e048cf1
skyline09130913 <139441311+skyline09130913@users.noreply.github.com>
2023-07-14 11:13:13 +0800

    Create Testdto.java

---
+++ src/main/java/com/example/task/dto/Testdto.java
@@ -0,0 +1,18 @@
+package com.example.task.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Testdto {
+
+    String a;
+    String b;
+    int c;
+    int dsx;
+}
